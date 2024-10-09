b2d2c8582307e4f9d7c57f2a6f5aac08cebbcba7
Farhoud-Rand <rand.farhoud2000@gmail.com>
2024-10-09 10:12:34 +0300

    ERC 721: Create mintable token with auto increment ID

--- ERC tokens/ERC 721/src/MyToken.sol
+++ ERC tokens/ERC 721/src/MyToken.sol
@@ -24,3 +24,21 @@ contract MyToken1 is ERC721, Ownable {
         _mint(to, tokenId);
     }
 }
+
+// ! Create mintable token with auto increment ID 
+contract MyToken2 is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    // * Implement the 2 parent constructors
+    constructor(address initialOwner)
+        ERC721("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
+
+    // * Mint function to add new tokens and assign ownership of those tokens to the address that create the token (deployed the contract)
+    function mint(address to) public onlyOwner {
+        // todo: Note we use _mint for minting without safe checks, to make the contract able to handle tokens without check them
+        uint256 tokenId = _nextTokenId++;
+        _mint(to, tokenId);
+    }
+}
--- ERC tokens/ERC 721/test/MyToken.t.sol
+++ ERC tokens/ERC 721/test/MyToken.t.sol
@@ -54,3 +54,52 @@ contract MyToken1Test is Test {
         token.mint(user, 0);
     }
 }
+
+// ! Test mintable token with auto increment ID 
+contract MyToken2Test is Test {
+    MyToken2 private token;
+
+    address private owner; // Address for the contract owner
+    address private user;  // Address for another user
+
+    function setUp() public {
+        owner = address(this); // Set the test contract as the owner
+        user = address(0x123); // Simulate another user address
+
+        // Deploy the MyToken2 contract with the test contract as the initial owner
+        token = new MyToken2(owner);
+    }
+
+    // * Test token name and symbol
+    function testTokenNameAndSymbol() public view {
+        // Check that the token name is correct
+        assertEq(token.name(), "MyToken");
+        // Check that the token symbol is correct
+        assertEq(token.symbol(), "MTK");
+    }
+
+    // * Test minting function by the owner
+    function testMintByOwner() public {
+        // Mint a token to the owner
+        token.mint(owner);
+        uint256 tokenId = 0; // Since this is the first token, ID should be 0
+
+        // Check the owner of token ID 0
+        assertEq(token.ownerOf(tokenId), owner);
+
+        // Mint a second token to the user
+        token.mint(user);
+        uint256 secondTokenId = 1; // The second token should have ID 1
+
+        // Check the owner of token ID 1
+        assertEq(token.ownerOf(secondTokenId), user);
+    }
+
+    // * Test minting is restricted to the contract owner
+    function testMintByNonOwner() public {
+        // Expect a revert when a non-owner tries to mint
+        vm.prank(user); // Simulate calling from the user address
+        vm.expectRevert("Ownable: caller is not the owner"); // Expect revert
+        token.mint(user);
+    }
+}

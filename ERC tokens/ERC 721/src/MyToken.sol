ef5b35c14a41abe5091e7e6ffe94c3f7d0b90fb7
Rand <r.farhoud2000@gmail.com>
2024-10-09 10:30:07 +0300

    ERC 721: Create mintable token with auto increment ID

--- ERC tokens/ERC 721/src/MyToken.sol
+++ ERC tokens/ERC 721/src/MyToken.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 // ! Create basic token
 contract MyToken is ERC721 {
     // * Implement the parent constructor 
--- ERC tokens/ERC 721/test/MyToken.t.sol
+++ ERC tokens/ERC 721/test/MyToken.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/MyToken.sol";
-
+ 
 // ! Test basic token
 contract MyTokenTest is Test {
     MyToken private token;
